8da5f88a8241113bbc566a3a9abacb4d416623fa
Marina <marinabrito.ma@gmail.com>
2021-02-26 11:40:16 +0000

    variable

---
+++ Variable/variableTypes.js
@@ -0,0 +1,18 @@
+// Numbers
+let myAge = 28;
+console.log(myAge);
+
+// Strings (Text)
+let cupGoodBye = 'Thanks for the coffee';
+console.log(cupGoodBye);
+
+// Booleans (true or false)
+let test = 6 < 3;
+console.log(test) // false
+let test2 = 10 > 5;
+console.log(test2); // true
+let life = true;
+console.log(life); //true
+let death = false;
+console.log(death); // false
+
