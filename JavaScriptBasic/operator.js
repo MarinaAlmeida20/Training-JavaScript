9214dec1e7421f732c08fedb2fcec4d63e9cb4be
Marina <marinabrito.ma@gmail.com>
2021-02-26 16:51:20 +0000

    js

---
+++ JavaScriptBasic/operator.js
@@ -0,0 +1,17 @@
+
+let num1 = 10;
+let num2 = 50;
+console.log( 9 * num1); // 90
+console.log(num1 ** 3); // 1000
+console.log(num2 / num1); // 5
+
+console.log(5 + 10 * 3); // 35
+console.log(num2 % 9 * num1); // 50
+console.log(num2 + num1 / 8 + 2); // 53.25
+
+// Operator precedence
+console.log((num2 + num1) / (8 + 2)); // 6
+
+// Increment and decrement operators
+// increment (++)
+// decrement (--)
