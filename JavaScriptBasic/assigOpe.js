c1c638274e23c25f1ace0f683a4801d868d1c101
Marina <marinabrito.ma@gmail.com>
2021-02-27 23:37:20 +0000

    comparison operators.

---
+++ JavaScriptBasic/assigOpe.js
@@ -0,0 +1,14 @@
+// Assignment operators ( = )
+let x = 3;
+let y = 4;
+//x = y;
+//console.log(x); // x = 4
+
+// ---------------------------------------
+
+x += 4;  // or  x = x + 4;
+x -= 3;  // or  x = x - 3;
+x *= 3;  // or  x = x * 3;
+x /= 5;  // or  x = x / 5;
+
+x *= y; // 12
---
+++ JavaScriptBasic/compOper.js
@@ -0,0 +1,7 @@
+// Comparison Operators
+5 === 2 + 4; // Strict equality
+5 !== 2 + 3; // Strict- non - equality
+10 < 6; // Less than
+10 > 20; // Greater than
+3 <= 2; // Less than or equal to
+5 >= 4; // Greater than or equal to
