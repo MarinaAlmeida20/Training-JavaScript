8dca65347c8e57d1d1a4cf6016ee72ba3a70e4fa
Marina <marinabrito.ma@gmail.com>
2021-02-28 16:26:53 +0000

    Number and Strings

---
+++ Variable/numAndString.js
@@ -0,0 +1,23 @@
+// Number and String
+let myDate = '19' + '67';
+typeof myDate;
+console.log(typeof myDate); // string
+console.log(myDate); // 1967
+
+// => String to number
+let myString = '123';
+let myNum = Number(myString);
+typeof myNum;
+console.log(typeof myString); // string
+console.log(typeof myNum); // number
+console.log(myNum); // 123
+
+// => Number to string
+let myNum2 = 123;
+let myString2 = myNum2.toString();
+typeof myString2;
+console.log(typeof myString2); // string
+console.log(typeof myNum2); // number
+console.log(myString2) // 123
+
+// Finding the length of a string
